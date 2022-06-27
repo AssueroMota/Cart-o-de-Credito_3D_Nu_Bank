c32bf834d2c4bfdea3aa29c833be47b9992fe8dd
AssueroMota <105565497+AssueroMota@users.noreply.github.com>
2022-06-27 13:09:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+    const card = document.querySelector('.container');
+
+    card.addEventListener("mousemove", efeitoCard);
+    card.addEventListener("mouseleave", tirarmouse);
+    card.addEventListener("mouseenter", clickmouse);
+
+    function efeitoCard(event)
+    {
+        const cardWidth = card.offsetWidth;
+        const cardHeight = card.offsetHeight;
+        const centerX = card.offsetLeft + cardWidth/2;
+        const centerY = card.offsetTop + cardHeight/2;
+        const positionX = event.clientX - centerX;
+        const positionY = event.clientY - centerY;
+        const rotateX = ((+1)*25*positionY/(cardHeight/2)).toFixed(2);
+        const rotateY = ((-1)*25*positionX/(cardWidth/2)).toFixed(2);
+
+        console.log(rotateX,rotateY);
+
+        card.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+    }
+
+    function tirarmouse(event)
+    {
+        card.style.transform = `perspective(500px) rotateX(0deg) rotateY(0deg)`;
+        cardTransition();
+    }
+
+    function cardTransition()
+    {
+        clearInterval(card.transitionId);
+        card.style.transition = 'transform 400ms';
+        card.transitionId = setTimeout(() => {
+            card.style.transition = '';
+        },400);
+    }
+
+    function clickmouse(event)
+    {
+        cardTransition();
+    }
+
+
